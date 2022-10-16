cd83411bc3b07a083039620bd8003b91936e752e
Nikita <nikita_rs7@ukr.net>
2022-10-17 01:21:03 +0300

    ffff

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const isfree = prompt("Enter free or something and will see result", '');
 
 let free = isfree.includes('free');
+if (free === true) {
+   alert(`Результат : ${free}`);
+}
+else {
+   alert(`Free не найден , результат : ${free}`);
+}
 
-alert(`Результат : ${free}`);
+
 
 
